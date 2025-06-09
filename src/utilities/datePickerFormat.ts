6eed8cb14d247d4ea8743943e9bc425950fb2694
Ibrahim <ibrv.cvrtxxn@gmail.com>
2025-06-10 01:35:54 +0200

    small fix to avoid fetching bugs

--- src/utilities/datePickerFormat.ts
+++ src/utilities/datePickerFormat.ts
@@ -2,5 +2,7 @@ export function datePickerFormat() {
   const currentDate = new Date();
   return `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1)
     .toString()
-    .padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`;
+    .padStart(2, '0')}-${(currentDate.getDate() - 1)
+    .toString()
+    .padStart(2, '0')}`;
 }
